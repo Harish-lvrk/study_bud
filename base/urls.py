bb578ad62512c9f2df6c5fecb9af923ea3b12f14
Harish-lvrk <lottiharish@gmail.com>
2026-02-18 15:09:58 +0530

    Complted the registration form

--- base/urls.py
+++ base/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('create-room/',views.createRoom, name = "create-room"),
     path('update-room/<str:pk>/',views.updateRoom, name = "update-room"),
     path('delete-room/<str:pk>/',views.deleteRoom, name = "delete-room"),
+    path('register/',views.registerPage, name = "register"),
     path('login/',views.loginPage, name = "login"),
+    
     path('logout/',views.logoutUser, name = "logout"),
+
+
 ]
+
--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Room, Topic
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .froms import RoomForm
@@ -92,10 +93,11 @@ from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
 def loginPage(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower() # this is used to get the data from the model and put it in the form 
         password = request.POST.get('password')
 
         user = authenticate(request, username=username, password=password)
@@ -107,7 +109,33 @@ def loginPage(request):
             messages.error(request, 'Username or password is incorrect')
             return redirect('login')
 
-    return render(request, 'base/login_register.html')
+    context = {'page':page}
+    return render(request, 'base/login_register.html', context)
+
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) # this is used to save the data from the model and put it in the form 
+            # here we use to save in the user because if we want to access the user right away
+            user.username = user.username.lower()
+            # we have to check is the username is alredy taken from the data base if yes 
+            # we need to refill the values and pop up the message with the user name is alredy taken
+           
+
+            user.save()
+            login(request, user) #before redirecting we are logging the user 
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during the registration')
+            return redirect('register')
+    context = {'form':form}
+    return render(request, 'base/login_register.html', context)
+
 
 def logoutUser(request):
     logout(request)
